3ec4555ba4a2360b51824dbb03681568bd9c551c
akangshya dutta <142735695+akangshyya@users.noreply.github.com>
2025-11-03 05:08:27 +0530

    Update 13. Longest subarray with given sum.cpp

--- 1. Array/13. Longest subarray with given sum.cpp
+++ 1. Array/13. Longest subarray with given sum.cpp
@@ -1,19 +1,18 @@
-    int start = 0;
-    int ans = 0;
-    long long sum = 0;
-    int n = a.size();
-
-    for (int end = 0; end < n; end++)
-    {
-        sum += a[end];
-        while (sum > k)
-        {
-            sum -= a[start];
-            start++;
-        }
-        if (sum == k)
-        {
-            ans = max(ans, end - start + 1);
-        }
+// 'A' = [1, 2, 3, 1, 1, 1, 1]
+// Output: 3
+// Explanation: Subarrays whose sum = '3' are:
+// [1, 2], [3], [1, 1, 1], [1, 1, 1]
+int start = 0;
+int ans = 0;
+long long sum = 0;
+int n = a.size();
+for (int i = 0; i < n; i++){
+    sum += a[i];
+    while (sum > k){
+        sum -= a[start];
+        start++;
     }
-    return ans;
+    if (sum == k)
+        ans = max(ans, i - start + 1);
+}
+return ans;
