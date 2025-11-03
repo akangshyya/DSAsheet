a7df72d8e3588a6b4845fc9f17e3a289702c5a0e
akangshya dutta <142735695+akangshyya@users.noreply.github.com>
2025-11-04 01:51:06 +0530

    Create 36. Count Inversion.cpp

---
+++ 1. Array/36. Count Inversion.cpp
@@ -0,0 +1,10 @@
+/*
+Given an array of integers. Find the Inversion Count in the array.
+Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If the array is already sorted then the inversion count is 0.
+
+Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
+*/
+//1st approach : 2 loops. next loop from i+1.
+//2nd approach: 
