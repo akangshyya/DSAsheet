8161fbea25f51aac13d6bf38893ca7bae42cf09a
akangshya dutta <142735695+akangshyya@users.noreply.github.com>
2025-11-05 01:43:35 +0530

    Create 01. Find x in sorted array.cpp

---
+++ 02. Binary Search/01. Find x in sorted array.cpp
@@ -0,0 +1,18 @@
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+
+
+//binary search algorithm
+int search(vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] > target)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
