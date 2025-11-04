b2ed49b4d005fc024e757f1250fbfbab573ce460
akangshya dutta <142735695+akangshyya@users.noreply.github.com>
2025-11-05 01:48:48 +0530

    Create 02. Implement lower bound.cpp

---
+++ 02. Binary Search/02. Implement lower bound.cpp
@@ -0,0 +1,17 @@
+// x = 5
+// arr[] = {1,2,8,10,11,12,19}
+// Output: 1
+//Largest number less than 5 is 2.
+
+long long low = 0, high = n - 1;
+int ans = -1;
+while (low <= high) {
+    long long mid = low + (high - low) / 2;
+    if (v[mid] <= x) {
+        ans = mid;
+        low = mid + 1;
+    } else {
+        high = mid - 1;
+    }
+}
+return ans;
